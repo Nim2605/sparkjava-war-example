6154d4456cbf09ddab1bd84895424db888fa6248
Nim2605 <127940190+Nim2605@users.noreply.github.com>
2025-02-11 03:08:43 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,6 @@
-//import spark.servlet.SparkApplication;
-
-//import static spark.Spark.get;
-
-//public class HelloWorld implements SparkApplication {
+import spark.servlet.SparkApplication;
+import static spark.Spark.get;
+public class HelloWorld implements SparkApplication {
 	public static void main(String[] args) {
 		new HelloWorld().init();
 	}
